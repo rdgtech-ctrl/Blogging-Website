8b320aaa9126cf5a87d54e5b0065f22efd5ab1ef
Disha <dg.mechpro200@gmail.com>
2026-04-29 22:25:34 +0530

    Update: 2026-04-29 22:25:32

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className="py-2 fixed w-full dark:bg-gray-800 dark:border-b-gray-600 border-b-gray-300 border-2 bg-white z-50">
+            <div className="max-w-7xl mx-auto flex justify-between items-center px-4 md:px-0">
+                {/* logo section */}
+                <div className='flex gap-7 items-center'>
+                    <Link>
+                        <div className="flex gap-2 items-center">
+                            <img src="" alt="" />
+                            <h1>Logo</h1>
+                        </div>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
